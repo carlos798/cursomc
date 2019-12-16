1913739ff1fb5b763b1827aa54569b7fce0afe7e
Carlos Alonso <carlos.alonso.email@gmail.com>
2019-12-16 12:21:14 -0300

    Teste Repositoio

--- src/main/java/com/carlosalonso/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/carlosalonso/cursomc/resources/CategoriaResource.java
@@ -1,18 +1,33 @@
 package com.carlosalonso.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.carlosalonso.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "Rest está funcionando";
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
+		
 	}
+	
 	
 
 }
